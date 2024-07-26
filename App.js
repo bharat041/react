79ce5a75ddb4c9a1014d89c0c31728006030b706
bharat041 <90520117+bharat041@users.noreply.github.com>
2024-07-26 18:55:48 +0530

    Added package and git ignore files

--- App.js
+++ App.js
@@ -9,19 +9,23 @@
         </div>
     </div> */}
 
-const parent = React.createElement("div",{id:"parent"},
-        React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"Hello I'm a h1 tag"),        React.createElement("h2",{},"Hello I'm a h2 tag"),
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "Hello I'm a h1 tag"), React.createElement("h2", {}, "Hello I'm a h2 tag"),
         ],
-        React.createElement("div",{id:"child"},
-            [React.createElement("h1",{},"Hello I'm a h1 tag"),
-            React.createElement("h2",{},"Hello I'm a h2 tag"),
+        React.createElement("div", { id: "child" },
+            [React.createElement("h1", {}, "Hello I'm a h1 tag"),
+            React.createElement("h2", {}, "Hello I'm a h2 tag"),
             ]
 
+        )
     )
 )
-)
-;
+    ;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
